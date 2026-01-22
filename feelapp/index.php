465ef691366ddda5f18cb6acd1148acce4ef88a2
Daniel Barreiros <danielbarreiros@MacBook-Air.local>
2026-01-22 15:55:49 +0000

    test

--- feelapp/index.php
+++ feelapp/index.php
@@ -527,7 +527,7 @@ if ($array_contenidos_insertados) {
 if ($_SESSION['proyecto'] == 'oasis') {
     $array_idiomas = array(1 => "español", 2 => "english");
 } else {
-    $array_idiomasx = array(1 => "es", 2 => "en", 3 => "de", 4 => "fr");
+    $array_idiomas = array(1 => "es", 2 => "en");
 }
 $total_idiomas = count($array_idiomas);
 
